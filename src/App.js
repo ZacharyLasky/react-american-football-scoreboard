b8dd6f4b49deb20ac09dfa5323e9d47491d83cab
Zach Lasky <zacharylasky123@gmail.com>
2019-07-15 18:23:54 -0400

    changed title and added h1 saying Scoreboard

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
-import Timer from "./Timer"
+import Timer from "./Timer";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
@@ -27,17 +27,17 @@ function App() {
         </div>
         <BottomRow />
       </section>
-      <section className="buttons">
-        <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button onClick={ ()=> setValueLions(valueLions +7)} className="homeButtons__touchdown">Home Touchdown</button>
-          <button onClick={ ()=> setValueLions(valueLions +3)} className="homeButtons__fieldGoal">Home Field Goal</button>
-        </div>
-        <div className="awayButtons">
-          <button onClick={ ()=> setValueTigers(valueTigers +7)} className="awayButtons__touchdown">Away Touchdown</button>
-          <button onClick={ ()=> setValueTigers(valueTigers +3)} className="awayButtons__fieldGoal">Away Field Goal</button>
-        </div>
-      </section>
+       <section className="buttons">
+      <div className="homeButtons">
+        {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
+        <button onClick={ ()=> setValueLions(valueLions +7)} className="homeButtons__touchdown">Home Touchdown</button>
+        <button onClick={ ()=> setValueLions(valueLions +3)} className="homeButtons__fieldGoal">Home Field Goal</button>
+      </div>
+      <div className="awayButtons">
+        <button onClick={ ()=> setValueTigers(valueTigers +7)} className="awayButtons__touchdown">Away Touchdown</button>
+        <button onClick={ ()=> setValueTigers(valueTigers +3)} className="awayButtons__fieldGoal">Away Field Goal</button>
+      </div>
+    </section>
     </div>
   );
 }
